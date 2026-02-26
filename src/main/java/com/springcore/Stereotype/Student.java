8f21b0bb9e2f5452a895aed8b0e3c699feb1b92d
kg956113 <kg956113@gmail.com>
2026-02-26 23:38:15 +0530

    stereotype annotation

---
+++ src/main/java/com/springcore/Stereotype/Student.java
@@ -0,0 +1,49 @@
+package com.springcore.Stereotype;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component("Ob")
+public class Student {
+    @Value("Komal raj")
+    private String studentName;
+    @Value("Banglore")
+    private String city;
+    @Value("#{temp}")
+    private List<String> address;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public List<String> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<String> address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentName='" + studentName + '\'' +
+                ", city='" + city + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
